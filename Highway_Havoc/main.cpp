fbfa8136d58bedafdadbcf822e1ef5a2a0df273d
felix <felix@FELIX-LAPTOP>
2023-12-09 22:52:31 +0100

    sfml hizugefügt

--- Highway_Havoc/main.cpp
+++ Highway_Havoc/main.cpp
@@ -1,3 +1,19 @@
+#include "SFML/Graphics.hpp"
+
 int main() {
+	//Fenster erstellen
+	sf::RenderWindow whandle(sf::VideoMode(800, 800), "Highway Havoc");
+	//Fenster updaten
+	while (whandle.isOpen()) {
+		sf::Event event;
+		while (whandle.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed) whandle.close();
+		}
+
+		whandle.clear(sf::Color::Black);
+
+		whandle.display();
+	}
 	return 0;
 }

163416cd02e379236c1199e37409e0eb33dfd6b6
LasseHuhn <Lasse.Huhn2@schueler.max-eyth-schule.de>
2024-05-20 23:53:04 +0200

    Banane Kommentiert

--- Highway_Havoc/src/Objekte/Bananenschale/Bananenschale.cpp
+++ Highway_Havoc/src/Objekte/Bananenschale/Bananenschale.cpp
@@ -1,52 +1,73 @@
 #pragma once
 #include "Bananenschale.hpp"
 
+// Destruktor für die Klasse Bananenschale
 Bananenschale::~Bananenschale()
 {
 }
 
+// Konstruktor für die Klasse Bananenschale
 Bananenschale::Bananenschale(sf::RenderWindow* window)
 {
-	this->window = window;
-	if (!this->bananenschaleTextur.loadFromFile("./res/grafiken/bananenschale.png")) {
-		std::cout << "Fehler beim Laden der Datei! (./res/grafiken/bananenschale.png)" << std::endl;
-	}
-	this->bananenschaleSprite.setTexture(this->bananenschaleTextur);
-	this->bananenschaleSprite.setOrigin(this->bananenschaleSprite.getGlobalBounds().getSize().x / 2, this->bananenschaleSprite.getGlobalBounds().getSize().y / 2);
-	float faktor = 50 / this->bananenschaleSprite.getGlobalBounds().getSize().x;
-	this->bananenschaleSprite.setScale(faktor, faktor);
-	this->bananenschaleSprite.setRotation(rand()%360);
+    // Weist das Fensterzeiger der Klassenvariable zu
+    this->window = window;
+
+    // Lädt die Textur für die Bananenschale und überprüft auf Fehler
+    if (!this->bananenschaleTextur.loadFromFile("./res/grafiken/bananenschale.png")) {
+        std::cout << "Fehler beim Laden der Datei! (./res/grafiken/bananenschale.png)" << std::endl;
+    }
+
+    // Setzt die Textur für den Sprite
+    this->bananenschaleSprite.setTexture(this->bananenschaleTextur);
+
+    // Setzt den Ursprung des Sprites auf dessen Zentrum
+    this->bananenschaleSprite.setOrigin(this->bananenschaleSprite.getGlobalBounds().getSize().x / 2, this->bananenschaleSprite.getGlobalBounds().getSize().y / 2);
+
+    // Skaliert den Sprite auf eine Breite von 50 Einheiten, wobei das Seitenverhältnis beibehalten wird
+    float faktor = 50 / this->bananenschaleSprite.getGlobalBounds().getSize().x;
+    this->bananenschaleSprite.setScale(faktor, faktor);
+
+    // Setzt eine zufällige Rotation für den Sprite
+    this->bananenschaleSprite.setRotation(rand() % 360);
 }
 
+// Aktualisierungsfunktion, derzeit leer
 void Bananenschale::aktualisieren()
 {
 }
 
+// Zeichnet den Bananenschalen-Sprite auf das Fenster
 void Bananenschale::anzeigen()
 {
-	this->window->draw(this->bananenschaleSprite);
+    this->window->draw(this->bananenschaleSprite);
 }
 
+// Gibt die Position des Bananenschalen-Sprites zurück
 sf::Vector2f Bananenschale::getPos()
 {
-	return this->bananenschaleSprite.getPosition();
+    return this->bananenschaleSprite.getPosition();
 }
 
+// Setzt die Position des Bananenschalen-Sprites
 void Bananenschale::setPos(sf::Vector2f position)
 {
-	this->bananenschaleSprite.setPosition(position);
+    this->bananenschaleSprite.setPosition(position);
 }
 
+// Gibt die globalen Begrenzungen des Bananenschalen-Sprites zurück
 sf::FloatRect Bananenschale::getGlobalBounds()
 {
-	return this->bananenschaleSprite.getGlobalBounds();
-}
-bool Bananenschale::kollidiert(sf::FloatRect viereck)
-{
-	return this->bananenschaleSprite.getGlobalBounds().intersects(viereck);
+    return this->bananenschaleSprite.getGlobalBounds();
 }
 
+// Überprüft, ob der Bananenschalen-Sprite mit einem gegebenen Rechteck kollidiert
+bool Bananenschale::kollidiert(sf::FloatRect viereck)
+{
+    return this->bananenschaleSprite.getGlobalBounds().intersects(viereck);
+}
+
+// Gibt einen Zeiger auf den Bananenschalen-Sprite zurück
 sf::Sprite* Bananenschale::getSprite()
 {
-	return &this->bananenschaleSprite;
+    return &this->bananenschaleSprite;
 }
--- Highway_Havoc/src/Objekte/Bananenschale/Bananenschale.hpp
+++ Highway_Havoc/src/Objekte/Bananenschale/Bananenschale.hpp
@@ -4,17 +4,17 @@
 
 class Bananenschale {
 private:
-	sf::RenderWindow* window;
-	sf::Texture bananenschaleTextur;
-	sf::Sprite bananenschaleSprite;
+	sf::RenderWindow* window; // Zeiger auf das SFML-Fenster
+	sf::Texture bananenschaleTextur; // Textur für die Bananenschale
+	sf::Sprite bananenschaleSprite; // Sprite für die Bananenschale
 public:
-	~Bananenschale();
-	Bananenschale(sf::RenderWindow* window);
-	void aktualisieren();
-	void anzeigen();
-	sf::Vector2f getPos();
-	void setPos(sf::Vector2f position);
-	sf::FloatRect getGlobalBounds();
-	bool kollidiert(sf::FloatRect viereck);
-	sf::Sprite* getSprite();
+	~Bananenschale(); // Destruktor
+	Bananenschale(sf::RenderWindow* window); // Konstruktor, der ein Fenster erwartet
+	void aktualisieren(); // Funktion zum Aktualisieren der Bananenschale
+	void anzeigen(); // Funktion zum Anzeigen der Bananenschale im Fenster
+	sf::Vector2f getPos(); // Funktion zum Abrufen der Position der Bananenschale
+	void setPos(sf::Vector2f position); // Funktion zum Setzen der Position der Bananenschale
+	sf::FloatRect getGlobalBounds(); // Funktion zum Abrufen der globalen Grenzen der Bananenschale
+	bool kollidiert(sf::FloatRect viereck); // Funktion zur Kollisionserkennung mit einem übergebenen Rechteck
+	sf::Sprite* getSprite(); // Funktion zum Abrufen des Sprites der Bananenschale
 };
